c48768249d73764ec3233c08a255b54fa52d2ade
Jomiemma <enujekoi@gmail.com>
2025-08-19 15:41:26 +0100

    added links

--- src/reusables/FindMe.js
+++ src/reusables/FindMe.js
@@ -14,28 +14,28 @@ function FindMe() {
       <p className="link-txt">Find me online.</p>
       <div className="links">
         <a
-          href="https://wa.me/234XXXXXXXXXX"
+          href="https://wa.me/2348097202463"
           target="_blank"
           rel="noopener noreferrer"
         >
           <FaWhatsapp />
         </a>
         <a
-          href="https://twitter.com/yourhandle"
+          href="https://twitter.com/@fw_nu3l"
           target="_blank"
           rel="noopener noreferrer"
         >
           <FaTwitter />
         </a>
         <a
-          href="https://linkedin.com/in/yourhandle"
+          href="https://linkedin.com/in/emmanuel-enujeko"
           target="_blank"
           rel="noopener noreferrer"
         >
           <FaLinkedin />
         </a>
         <a
-          href="https://github.com/yourhandle"
+          href="https://github.com/Jomiemma"
           target="_blank"
           rel="noopener noreferrer"
         >
